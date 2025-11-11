3577f4f9291c1fae2a189adb46ff865941c89eb9
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-11-11 15:34:16 +0800

    a lot of games

---
+++ docs/globals.js
@@ -0,0 +1,23 @@
+SHADOW = new Color(0,0,0,.2)
+PAL = {
+  'R': [
+    new Color(.9,.6,.2), // light
+    new Color(.9,.4,.1), // med
+    new Color(.4,.2,.1) // dark
+  ],
+  'G': [
+    new Color(.3,.7,.2),
+    new Color(.2,.5,.1),
+    new Color(.2,.3,.2)
+  ],
+  'B': [
+    new Color(.7, .86, .9),
+    new Color(.2, .6, .9),
+    new Color(0, .3, .5)
+  ],
+  'S': [
+    new Color(.9,.9,.8),
+    new Color(.6,.6,.5),
+    new Color(.3,.3,.2)
+  ]
+}
